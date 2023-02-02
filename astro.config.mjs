87a39abe076ad122509e28fb47432a29fe9d2330
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2023-02-02 21:18:11 +0000

    added vercel adapt

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), preact(), react()]
+  integrations: [mdx(), tailwind(), preact(), react()],
+  output: "server",
+  adapter: vercel()
 });
