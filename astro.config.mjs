5a3c694f50b29f5bfe8544fa7c67cfde584f6372
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2023-02-02 21:45:35 +0000

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
@@ -8,12 +8,9 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), preact(), react()],
-  output: "server",
-  adapter: vercel()
 });
