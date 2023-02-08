d99ee00dd88fe24646db7e3d87a633b5716c957b
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2023-02-08 11:20:50 +0000

    added translations to tabs

--- src/components/WorkTab.tsx
+++ src/components/WorkTab.tsx
@@ -1,34 +1,29 @@
+import { t } from "i18next";
 import { TabsList, TabsTrigger, Tabs, TabsContent } from "./ui/Tabs";
 
 export default function WorkTab() {
+  const categories = [
+    { key: "all", value: t("works.all") },
+    { key: "webdesign", value: t("works.webdesign") },
+    { key: "illustration", value: t("works.illustration") },
+    { key: "pictorgrams", value: t("works.pictograms") },
+    { key: "branding", value: t("works.branding") },
+    { key: "packaging", value: t("works.packaging") },
+    { key: "infography", value: t("works.infography") },
+  ];
+
   return (
-    <Tabs defaultValue="todos">
+    <Tabs defaultValue="all">
       <div className="flex justify-center">
         <TabsList className="flex-wrap gap-4 bg-none">
-          <TabsTrigger value="todos" className="">
-            Todos
-          </TabsTrigger>
-          <TabsTrigger value="webdesign" className="">
-            Webdesign
-          </TabsTrigger>
-          <TabsTrigger value="ilustração" className="">
-            Ilustração
-          </TabsTrigger>
-          <TabsTrigger value="pictorgramas" className="">
-            Pictogramas
-          </TabsTrigger>
-          <TabsTrigger value="branding" className="">
-            Branding
-          </TabsTrigger>
-          <TabsTrigger value="packaging" className="">
-            Packaging
-          </TabsTrigger>
-          <TabsTrigger value="infografia" className="">
-            Infografia
-          </TabsTrigger>
+          {categories.map((category) => (
+            <TabsTrigger key={category.key} value={category.key}>
+              {category.value}
+            </TabsTrigger>
+          ))}
         </TabsList>
       </div>
-      <TabsContent value="todos" className="pt-10">
+      <TabsContent value="all" className="pt-10">
         <div className="grid grid-cols-4 grid-rows-4 place-items-center gap-5">
           <h1>
             <a href="/trabalhos/hi">h1</a>
@@ -41,11 +36,11 @@ export default function WorkTab() {
         </div>
       </TabsContent>
       <TabsContent value="webdesign">Partial</TabsContent>
-      <TabsContent value="ilustração">Partial</TabsContent>
-      <TabsContent value="pictorgramas">Partial</TabsContent>
+      <TabsContent value="illustration">Partial</TabsContent>
+      <TabsContent value="pictorgrams">Partial</TabsContent>
       <TabsContent value="branding">Partial</TabsContent>
       <TabsContent value="packaging">Partial</TabsContent>
-      <TabsContent value="infografia">Partial</TabsContent>
+      <TabsContent value="infography">Partial</TabsContent>
     </Tabs>
   );
 }
