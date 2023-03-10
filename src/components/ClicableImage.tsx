4a12796028fcd05a8747b25e87d6876a046e40ea
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2023-03-10 20:35:39 +0000

    fixing some stuff on about

--- src/components/ClicableImage.tsx
+++ src/components/ClicableImage.tsx
@@ -4,13 +4,22 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/Dialog";
 interface ClicableImageProps {
   thumbnail?: React.ReactNode;
   fullImage?: React.ReactNode;
+  classNamesThumb?: string;
 }
 
-const ClicableImage = ({ thumbnail, fullImage }: ClicableImageProps) => {
+const ClicableImage = ({
+  thumbnail,
+  fullImage,
+  classNamesThumb,
+}: ClicableImageProps) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="cursor-zoom-in snap-center">{thumbnail}</div>
+        <div
+          className={`max-h-screen cursor-zoom-in snap-center  ${classNamesThumb}`}
+        >
+          {thumbnail}
+        </div>
       </DialogTrigger>
       <DialogContent className="cursor-none md:w-3/5 md:max-w-none">
         {fullImage}
