735b48c8586717f5e992a154d1942510901bf8d8
jrp <jrpacheco.dev@gmail.com>
2023-04-12 17:48:59 +0100

    fix: change default theme to light. closes SUS-30

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -10,9 +10,6 @@ const ThemeToggle = () => {
     if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
       return localStorage.getItem("theme");
     }
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    }
     return "light";
   });
 
